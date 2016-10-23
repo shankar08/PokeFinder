7ae80858e757620ead76404846483848ac03408c
shankar08 <shan08kar@gmail.com>
2016-10-23 09:59:33 -0500

    completed 1

---
+++ PokeFinder/PokeFinder-Bridging-header.h
@@ -0,0 +1,9 @@
+//
+//  PokeFinder-Bridging-header.h
+//  PokeFinder
+//
+//  Created by Shankar Prajapati on 10/22/16.
+//  Copyright © 2016 Shankar Prajapati. All rights reserved.
+//
+
+#import "GeoFire.h"
